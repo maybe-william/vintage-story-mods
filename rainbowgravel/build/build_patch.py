5fe36e5cfa23d7b6349f7fbc249d0ba8e0e9285a
maybe-william <47429679+maybe-william@users.noreply.github.com>
2026-03-24 13:34:49 -0600

    Add build requirements file, also add jinja2 template scaffold for testing

--- rainbowgravel/build/build_patch.py
+++ rainbowgravel/build/build_patch.py
@@ -1,5 +1,43 @@
 #!/usr/bin/env python3
 import re
+from jinja2 import Template
+
+# type is "block" or "item"
+# code is the code of the block or item, with "game:" prefix ("stone-limestone")
+# chance is avg percentage in float form, leaving var: 0 for now.
+PATCH_FORMAT = Template("""
+[
+  {
+    "file": "game:blocktypes/wood/pan",
+    "op": "replace",
+    "path": "/attributes/panningDrops/@(sand|gravel|sandwavy)-.*",
+    "value": [
+    {% for item in drops %}
+      {
+        "type": "{{item[0]}}",
+        "code": "game:{{item[1]}}",
+        "chance": { "avg": {{item[2]}}, "var": 0 }
+      }
+    {% if not loop.last %},{% endif %}
+    {% endfor %}
+    ]
+  }
+]
+""")
+
+# from jinja2 import Template
+
+# template_string = """
+# My favorite fruits are:
+# {% for fruit in fruits %}
+# - {{ fruit }}
+# {% endfor %}
+# """
+# data = {"fruits": ["apple", "banana", "cherry"]}
+# output = Template(template_string).render(**data)
+# print(output.strip())
+
+
 
 items_blacklist = [
     "magicwand.*",
@@ -65,21 +103,35 @@ def get_counts(dic):
         counts[k] = len(v)
     return counts
 
-def simple_debug_counts(filename="all-item-codes"):
-    """A simple function to get counts for a file without the filtering and structurizing"""
+def get_filtered_list(filename="all-item-codes", item=True):
+    """Gets the filtered list of either items or blocks from a file"""
     with open(filename, "r") as f:
         item_codes = f.read().splitlines()
-        item_codes = [code for code in item_codes if filter(code, item=True)]
-        item_codes = structurize(item_codes)
-        item_codes = get_counts(item_codes)
-        print(item_codes)
+        item_codes = [code for code in item_codes if filter(code, item=item)]
+    return item_codes
+
+def simple_debug_counts(filtered_list):
+    """A simple function to get counts for a file without the filtering and structurizing"""
+    item_codes = structurize(filtered_list)
+    item_codes = get_counts(item_codes)
+    print(item_codes)
 
 if __name__ == "__main__":
     item_codes = {}
     block_codes = {}
 
-    # simple_debug_counts("all-item-codes")
-    # simple_debug_counts("all-block-codes")
+    item_list = get_filtered_list("all-item-codes", item=True)
+    block_list = get_filtered_list("all-block-codes", item=False)
+
+    drops = []
+
+    # simple_debug_counts(item_list)
+    # simple_debug_counts(block_list)
 
     # TODO: build out the actual patch with structurized item list
+
+    print(PATCH_FORMAT.render(drops=[
+        ("item", "stone-limestone", 0.30),
+        ("block", "metal-parts", 0.70)
+    ]))
 
